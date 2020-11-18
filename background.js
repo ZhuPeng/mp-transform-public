7e4f9b07b632d0e9a75a968dde088436adbdc9a1
Peng Zhu <zhupeng02@meituan.com>
2020-11-18 22:40:21 +0800

    Support download instagram igtv

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     var scope = "https://mp.weixin.qq.com/"
+    console.log('browserAction Clicked: ' + tab.url);
     if (tab.url.startsWith(scope)) {
-        console.log('browserAction Clicked: ' + tab.url);
         chrome.tabs.executeScript({
             file: 'contentScript.js'
+        });
+    } if (tab.url.startsWith("https://www.instagram.com/")) {
+        chrome.tabs.executeScript({
+            file: 'instagram.js'
         });
     } else {
         alert("插件只在微信公众平台（" + scope + "）生效")
---
+++ instagram.js
@@ -0,0 +1,25 @@
+var scripts = document.getElementsByTagName('script')
+console.log("scripts:", scripts)
+
+for (i=0; i<scripts.length; i++) {
+    console.log("script:", scripts[i])
+    var html = scripts[i].innerHTML
+    var tag = "window.__additionalDataLoaded("
+    if (html.indexOf(tag) == -1) {
+        continue
+    }
+
+    var tag = 'video_url":"'
+    var re = /video_url":"([^"]+?)"/g;
+    var myArray = html.match(re);
+    console.log(myArray);
+    var str = myArray[0]
+    var url = str.slice(tag.length, str.length-1).replace(/\\u0026/g, "&")
+    alert("下载视频链接：" + url)
+    window.open(url)
+}
+        
+function alertText(t) {
+    alert(t)
+    alert(t.slice(t.length-10, t.length))
+}
