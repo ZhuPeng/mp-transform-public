d7ae7f8fa79001d1cecc6a14f00ab66e4ca5c51a
Peng Zhu <zhupeng02@meituan.com>
2020-11-29 20:45:42 +0800

    Fix instagram tv download

--- instagram.js
+++ instagram.js
@@ -12,6 +12,7 @@ for (i=0; i<scripts.length; i++) {
     var tag = 'video_url":"'
     var re = /video_url":"([^"]+?)"/g;
     var myArray = html.match(re);
+    if (!myArray) {continue}
     console.log(myArray);
     var str = myArray[0]
     var url = str.slice(tag.length, str.length-1).replace(/\\u0026/g, "&")
