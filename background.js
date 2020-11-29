f27e3739091b11cf2d661df3b4b29b600f4e75d4
Peng ZHU <zhupengbupt@gmail.com>
2020-11-30 00:07:48 +0800

    Support fb.watch

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.executeScript({file: 'contentScript.js'});
     } else if (tab.url.startsWith("https://www.instagram.com/tv/")) {
         chrome.tabs.executeScript({file: 'instagram.js'});
-    } else if (tab.url.indexOf("facebook.com") > -1 || tab.url.indexOf("youtube.com") > -1) {
+    } else if (tab.url.indexOf("facebook.com") > -1 || tab.url.indexOf("youtube.com") > -1 || tab.url.indexOf('fb.watch') > -1) {
         chrome.tabs.executeScript({file: 'video.js'});
     } else if (tab.url.indexOf("en.savefrom.net")) {
         chrome.tabs.executeScript({file: 'savefrom.js'});
