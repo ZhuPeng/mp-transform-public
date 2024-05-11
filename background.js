0cd5e538d562634d9ec04352e608a66a313b3e3b
ZhuPeng <zhupengbupt@gmail.com>
2024-05-11 12:19:13 +0800

    add limit user

--- background.js
+++ background.js
@@ -1,6 +1,49 @@
+chrome.identity.getProfileUserInfo(function(userInfo) {
+    console.log(userInfo);
+    email = userInfo.email;
+
+    var isPaid = localStorage.getItem('isPaid') || false;
+    if (isPaid) {return}
+
+    fetch('https://raw.githubusercontent.com/ZhuPeng/mp-transform-public/master/.user')
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+            if (data.indexOf(email) > -1) {
+                console.log('user is paied')
+                localStorage.setItem('isPaid', true);
+                return false
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+
+function limitUsage() {
+    const date = new Date().toLocaleDateString();
+    var key = date + 'usageCount'
+    var usageCount = localStorage.getItem(key) || 0;
+    usageCount++;
+    localStorage.setItem(key, usageCount);
+
+    if (usageCount <= 2) {
+        return false;
+    }
+
+    var isPaid = localStorage.getItem('isPaid') || false;
+    if (isPaid) {return false}
+
+    alert("超过使用限制，请联系管理员开通付费服务\n用户：" + email + "\n管理员（微信）：15652961268\n")
+    return true
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     var scope = "https://mp.weixin.qq.com/"
     console.log('browserAction Clicked: ' + tab.url);
+    if (limitUsage() === true) {return}
+
     if (tab.url.startsWith(scope)) {
         chrome.tabs.executeScript({file: 'contentScript.js'});
     } else if (tab.url.startsWith("https://www.instagram.com/tv/")) {
