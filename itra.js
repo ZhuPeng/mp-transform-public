60cba45f7872d1bbedfa6d2cff869c6213b47a4d
PengZHU <zhupeng@PengZHUdeMBP.lan>
2024-10-12 00:25:32 +0800

    Use dict to map itra score

--- itra.js
+++ itra.js
@@ -1,33 +1,40 @@
 window.addEventListener('load', function() {
     if (window.location.href.indexOf('itra') > -1) {
         showHiddenInfo()
-    }
+    } else if (window.location.href.indexOf('itra.run/api/RunnerSpace/GetRunnerSpace') > -1 && document.body.innerText.indexOf('') > -1) {
+			  var url = '"https://itra.run/api/Race/GetRaceResultsData?runnerId=2051623&pageNumber=1&pageSize=10&raceYear=&categoryId="'
+			  requestRaceResults(url);
+		}
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'modifyData') {
       console.log('Received requests details', request.details);
-      var url = request.details.url;
-      if (url.indexOf('?') > -1) {
-          url = url + '&__not_listen__'
-      } else {
-          url = url + '?__not_listen__'
-      }
-      fetch(url)
-         .then(response => response.json())
-         .then(data => {
-             console.log('fetch data:', data);
-             showScore(data['raceResults']);
-         });
+			requestRaceResults(request.details.url);
     }
 });
+
+function requestRaceResults(url) {
+    if (url.indexOf('?') > -1) {
+        url = url + '&__not_listen__'
+    } else {
+        url = url + '?__not_listen__'
+    }
+    fetch(url)
+       .then(response => response.json())
+       .then(data => {
+           console.log('fetch data:', data);
+				   var dictionary = Object.assign({}, ...data['raceResults'].map((x) => ({[x.date+x.name.replace('  ', ' ').trim()]: x})));
+           showScore(dictionary);
+       });
+}
 
 function loopSelectXpath(xpath, modifyFunc) {
     var result = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
     var node = result.iterateNext()
     var nodesToModify = [];
     while (node) {
-        console.log('node:', node);
+        console.log('node:', node, 'children:', node.children);
         nodesToModify.push({node: node});
         node = result.iterateNext();
     }
@@ -38,18 +45,25 @@ function loopSelectXpath(xpath, modifyFunc) {
 
 function showScore(raceResults) {
     console.log('showScore:', raceResults);
-    loopSelectXpath("//div[@class='table-info text-700 race-score']/div[@class='d-flex locked']", function(index, node) {
-        node.classList.remove('locked');
-        if (node.innerText.trim() == "DNF") {
-            node.innerText = raceResults[index]['score'];
+    // loopSelectXpath("//div[contains(@class, 'table-info text-700 race-score')]/div[@class='d-flex locked']", function(index, node) {
+    loopSelectXpath("//div[@class='row table-body-row']", function(index, node) {
+			  var ch = node.children[8].children[1].children[0];
+			  if (!ch.classList.contains('locked')) {
+					  return;
         }
+			  var date = node.children[0].innerText.trim();
+			  var race = node.children[2].children[0].children[0].innerText.trim();
+				var score = 0;
+			  if (raceResults[date+race]) { score = raceResults[date+race]['score']; }
+				console.log('parsed:', date, race, score);
+
+        ch.classList.remove('locked');
+			  ch.innerText = score;
     });
 }
 
 const pTags = document.querySelectorAll('p');
-
 const button = Array.from(pTags).find(p => p.textContent.trim() === 'Time Charts');
-
 if (button) {
     button.addEventListener('click', function() {
         console.log('Time Charts clicked');
